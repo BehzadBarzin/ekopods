49b2ed09cefd12abb7d76a3e6afed2840304ed00
Behzad Barzin <behzad_barzin@live.com>
2024-06-05 15:36:00 +0330

    Changed Icon

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -18,7 +18,7 @@ const LeftSidebar = () => {
           href="/"
           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
         >
-          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <Image src="/icons/logo.png" alt="logo" width={23} height={27} />
           <h1 className="text-24 font-extrabold text-white max-lg:hidden">
             Ekopods
           </h1>
