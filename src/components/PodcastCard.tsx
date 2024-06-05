97164a88ee8462b74d9d97ebcd4d0fac80ff3e64
Behzad Barzin <behzad_barzin@live.com>
2024-06-05 17:13:39 +0330

    Defined podcast cards in the home page

---
+++ src/components/PodcastCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React, { FC } from "react";
+
+interface IProps {
+  podcastId: number;
+  imgUrl: string;
+  title: string;
+  description: string;
+}
+
+const PodcastCard: FC<IProps> = ({ podcastId, imgUrl, title, description }) => {
+  return (
+    <div className="cursor-pointer ">
+      <figure className="flex flex-col gap-2">
+        <Image
+          src={imgUrl}
+          alt={title}
+          width={174}
+          height={174}
+          className="aspect-square h-fit w-full rounded-xl 2xl:size-[200px]"
+        />
+        <div className="flex flex-col">
+          <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+          <h2 className="text-12 truncate font-normal text-white-4 capitalize">
+            {description}
+          </h2>
+        </div>
+      </figure>
+    </div>
+  );
+};
+
+export default PodcastCard;
